fe88f02ca075c81ff47ef6104f33c789f8edcbe1
zhenyuan992 <zhenyuan.yeo13@sps.nus.edu.sg>
2016-02-27 17:27:20 +0800

    Add editTaskTimeStart method in TaskManager class

--- TaskScheduler/src/TaskManager.java
+++ TaskScheduler/src/TaskManager.java
@@ -1,12 +1,13 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * This class is the main logic of the whole application, takes input from UI,
  * pass it into parser to get the command and then operate.
  * 
- *returns to UI and also saves
+ * returns to UI and also saves
  */
 public class TaskManager implements Serializable {
     private static final long serialVersionUID = 5891852874329459561L;
@@ -28,16 +29,22 @@ public class TaskManager implements Serializable {
     public static void deleteTaskAtIndex(int index) {
         tasks.remove(index);
     }
-    
+
     public static void editTaskNameAtIndex(int index, String newTaskName) {
         Task task = tasks.get(index);
         task.setName(newTaskName);
         tasks.set(index, task);
     }
-    
+
     public static void editTaskDurationAtIndex(int index, int newDuration) {
         Task task = tasks.get(index);
         task.setDuration(newDuration);
+        tasks.set(index, task);
+    }
+
+    public static void editTaskTimeStartAtIndex(int index, Date newTimeStart) {
+        Task task = tasks.get(index);
+        task.setTimeStart(newTimeStart);
         tasks.set(index, task);
     }
 
