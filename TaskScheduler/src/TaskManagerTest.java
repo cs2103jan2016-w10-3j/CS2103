e11844b085ad5d1285a00a84d36baa8fe739af6b
zhenyuan992 <zhenyuan.yeo13@sps.nus.edu.sg>
2016-04-06 02:29:39 +0800

    Added alertfunction in TaskManager. WARNING: only works after you add a task.

--- TaskScheduler/src/TaskManagerTest.java
+++ TaskScheduler/src/TaskManagerTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.After;
@@ -24,7 +26,7 @@ public class TaskManagerTest {
     public List<Task> workingTasks;
 
     @Before
-    public void initialise() {
+    public void initialise() throws FileNotFoundException, IOException {
         tm = new TaskManager();
         taskManager = TaskManager.getInstance();
         existingTasks = taskManager.existingList();
