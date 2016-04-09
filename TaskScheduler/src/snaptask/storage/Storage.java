ca799a03e9cb46433dd37d2b290f8e0d6445365b
zhenyuan992 <zhenyuan.yeo13@sps.nus.edu.sg>
2016-04-09 15:48:01 +0800

    Function to save and read file in human readable form.

--- TaskScheduler/src/snaptask/storage/Storage.java
+++ TaskScheduler/src/snaptask/storage/Storage.java
@@ -1,8 +1,11 @@
 package snaptask.storage;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +17,7 @@ import java.io.UnsupportedEncodingException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -25,12 +29,12 @@ public class Storage {
 	private ArrayList<Task> tasks;
 	private String fileName;
 	private String filePath;
-	
+	private StorageReadSave storageReadSave = null;
 	private final String STORE_FILE = "StoredName.txt";
 
 	public Storage() throws FileNotFoundException, IOException {
 		tasks = new ArrayList<Task>();
-				
+		storageReadSave = StorageReadSave.getInstance();
 		getStoredFileName();
 	}
 
@@ -132,45 +136,84 @@ public class Storage {
 
 	
 	
-	//Function to save tasks that are currently in task manager
-	@SuppressWarnings("unchecked")
-	public ArrayList<Task> readTasks() {
-		this.tasks = new ArrayList<Task>();
+//	//Function to save tasks that are currently in task manager
+//	@SuppressWarnings("unchecked")
+//	public ArrayList<Task> readTasks() {
+//		this.tasks = new ArrayList<Task>();
+//
+//		File tasksSaveFile = new File(filePath + fileName + ".con");
+//		if(tasksSaveFile.exists()) {
+//			try {
+//				FileInputStream fout = new FileInputStream(tasksSaveFile);
+//				ObjectInputStream oos = new ObjectInputStream(fout);
+//				this.tasks = (ArrayList<Task>) oos.readObject();
+//				oos.close();
+//
+//			} catch (Exception ex) {
+//				JOptionPane.showMessageDialog(null,
+//						"Unable to save the current configuration: " + ex.getMessage());
+//				ex.printStackTrace();
+//			}
+//		}
+//		return this.tasks;
+//	}
 
-		File tasksSaveFile = new File(filePath + fileName + ".con");
-		if(tasksSaveFile.exists()) {
-			try {
-				FileInputStream fout = new FileInputStream(tasksSaveFile);
-				ObjectInputStream oos = new ObjectInputStream(fout);
-				this.tasks = (ArrayList<Task>) oos.readObject();
-				oos.close();
+    // Function to read tasks from the file, called when the program initalise
+    public ArrayList<Task> readTasks() {
+        this.tasks = new ArrayList<Task>();
 
-			} catch (Exception ex) {
-				JOptionPane.showMessageDialog(null,
-						"Unable to save the current configuration: " + ex.getMessage());
-				ex.printStackTrace();
-			}
-		}
-		return this.tasks;
-	}
+        File tasksSavedFile = new File(filePath + fileName + ".con");
+        if (tasksSavedFile.exists()) {
+            try {
+                BufferedReader input = new BufferedReader(new FileReader(tasksSavedFile));
+                String currentLine = input.readLine();
+                while (currentLine != null) {
+                    tasks.add(storageReadSave.toTaskFromString(currentLine));
+                    currentLine = input.readLine();
+                }
+                input.close();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Unable to read from file: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+        return this.tasks;
+    }
 
+    // Function to save tasks in human form
+    public void saveTasks(List<Task> tasks) {
+        File tasksSavedFile = new File(filePath + fileName + ".con");
 
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(tasksSavedFile));
+            for (Task task : tasks) {
+                output.write(storageReadSave.toStringFromTask(task) + "\n");
+            }
+            output.close();
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null,
+                    "Unable to read the current configuration: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 	
-	//Function to save tasks 
-	public void saveTasks(Object taskManager) {
-		File tasksSaveFile = new File(filePath + fileName + ".con");
-		
-		try {
-			FileOutputStream fout = new FileOutputStream(tasksSaveFile);
-			ObjectOutputStream oos = new ObjectOutputStream(fout);
-			oos.writeObject(taskManager);
-			oos.close();
-
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null,
-					"Unable to read the current configuration: " + ex.getMessage());
-			ex.printStackTrace();
-		}
-	}
+//	//Function to save tasks 
+//	public void saveTasks(Object taskManager) {
+//		File tasksSaveFile = new File(filePath + fileName + ".con");
+//		
+//		try {
+//			FileOutputStream fout = new FileOutputStream(tasksSaveFile);
+//			ObjectOutputStream oos = new ObjectOutputStream(fout);
+//			oos.writeObject(taskManager);
+//			oos.close();
+//
+//		} catch (Exception ex) {
+//			JOptionPane.showMessageDialog(null,
+//					"Unable to read the current configuration: " + ex.getMessage());
+//			ex.printStackTrace();
+//		}
+//	}
 
 }
